bd03f33d3dda8a2b2e83f10fbb3950e21716e1b9
Thomas Kasbarian <thomas.kasbarian@gmail.com>
2024-06-15 12:00:01 +0200

    added board.max_width and game.player_turn

--- src/alcu.h
+++ src/alcu.h
@@ -1,6 +1,8 @@
 #ifndef ALCU_H
 # define ALCU_H
 
+#include <stdbool.h>
+
 # define SYS_ERROR_GNL		"System Error: Cannot read input!\n"
 # define SYS_ERROR_MALLOC 	"System Error: Cannot allocate memory!\n"
 # define ERROR_ARGUMENTS 	"Error: Invalid program arguments!\n"
@@ -16,21 +18,23 @@
 typedef unsigned long size_t;
 
 enum	e_game_status {
-	PLAYING,
-	WON,
-	LOST
+	PLAYING,	// game in progress...
+	WON,		// player won
+	LOST		// AI won
 };
 
 typedef struct s_board {
 	int		*heaps;
-	size_t	size;
-	size_t	capacity;
+	int		max_width;		// longest row(heap) in the board
+	size_t	size;			// number of rows(heaps) - matches will always be removed from heaps[size - 1]
+	size_t	capacity;		// this is for mem alloc...
 }			t_board;
 
 typedef struct s_game {
 	int	game_status;
 	t_board	board;
-	size_t	round;
+	size_t	round;			// start with 0 and increase at the beginning of each round
+	bool	player_turn;	// is it the players turn or the AI's turn? (start with false)
 }			t_game;
 
 // board.c:
