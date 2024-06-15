be2d33abd4b42b3b7ac4946f5206cad93283dc18
Benjamin Pochlauer <bpochlau@c4r3p9.42vienna.com>
2024-06-15 12:46:24 +0200

    added ai for one row + random before

---
+++ src/ai.c
@@ -0,0 +1,108 @@
+
+#include "alcu.h"
+#include <fcntl.h>
+#include <unistd.h>
+#include "./utils/utils.h"
+
+// Creates a random number between 1 and 3
+int	ft_random(t_game *game)
+{
+	int	num;
+	int	fd;
+
+	fd = open("/dev/random", O_RDONLY);
+	if (fd < 0)
+	{
+		ft_putstr_fd(2, "Error: Cannot open /dev/random\n");
+		return (-1);
+	}
+	if (read(fd, &num, 1) < 0)
+	{
+		ft_putstr_fd(2, "Error: Cannot read /dev/random\n");
+		close(fd);
+		return (-1);
+	}
+	close(fd);
+	num = num % 3 + 1;
+	if (num <= game->board.heaps[game->board.size - 1])
+		num -= game->board.heaps[game->board.size - 1];
+	else
+		game->board.heaps[game->board.size - 1] = 0;
+	return (0);
+}
+
+int	ft_calc_last_row(t_game *game)
+{
+	if (game->board.heaps[0] == 1)
+	{
+		game->board.heaps[0] = 0;
+		ft_putstr_fd(1, "AI took 1\n");
+	}
+	else if (game->board.heaps[0] == 2)
+	{
+		game->board.heaps[0] = 1;
+		ft_putstr_fd(1, "AI took 1\n");
+	}
+	else if (game->board.heaps[0] == 3)
+	{
+		game->board.heaps[0] = 1;
+		ft_putstr_fd(1, "AI took 2\n");
+	}
+	else if (game->board.heaps[0] == 4)
+	{
+		game->board.heaps[0] = 1;
+		ft_putstr_fd(1, "AI took 3\n");
+	}
+	else if (game->board.heaps[0] == 5)
+	{
+		game->board.heaps[0] = 4;
+		ft_putstr_fd(1, "AI took 1\n");
+	}
+	else
+	{
+		if ((game->board.heaps[0] - 5) % 4 == 0)
+			ft_random(game);
+		else if ((game->board.heaps[0] - 5) % 4 == 1)
+		{
+			game->board.heaps[0] -= 1;
+			ft_putstr_fd(1, "AI took 1\n");
+		}
+		else if ((game->board.heaps[0] - 5) % 4 == 2)
+		{
+			game->board.heaps[0] -= 2;
+			ft_putstr_fd(1, "AI took 2\n");
+		}
+		else if ((game->board.heaps[0] - 5) % 4 == 3)
+		{
+			game->board.heaps[0] -= 3;
+			ft_putstr_fd(1, "AI took 3\n");
+		}
+	}
+	return (0);
+}
+
+int	ft_calculate(t_game *game)
+{
+	if  (game->board.size == 1)
+	{
+		ft_calc_last_row(game);
+	}
+}
+
+int	ai(t_game *game)
+{
+	// The computer starts playing
+	// goal: leave the player with 1 heap
+
+	if (game->board.size > 1)
+	{
+		if (ft_random(game))
+			return (1);
+	}
+	else
+	{
+		if (ft_calculate(game))
+			return (1);
+	}
+	return (0);
+}
