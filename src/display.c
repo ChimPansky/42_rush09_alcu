7e7ce3e3dac88e437fd739452500fb165e076587
Maria Kamilla Huszar <mhuszar@c4r3p8.42vienna.com>
2024-06-15 12:29:40 +0200

    added update_board function

--- src/display.c
+++ src/display.c
@@ -6,16 +6,32 @@
 /*   By: mhuszar <mhuszar@student.42vienna.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/15 11:53:47 by mhuszar           #+#    #+#             */
-/*   Updated: 2024/06/15 12:03:08 by mhuszar          ###   ########.fr       */
+/*   Updated: 2024/06/15 12:29:12 by mhuszar          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 # include "alcu.h"
 
-bool update_board(t_game *game, int rm)
+bool update_board(t_board *board, int rm)
 {
     int counter;
 
     counter = 0;
-    
+    while (counter < board->size)
+    {
+        if (counter = board->size - 1
+            || (board->size > (counter + 1) && !board->heaps[counter + 1]))
+        {
+            if (rm <= board->heaps[counter])
+            {
+                board->heaps[counter] -= rm;
+                return (true);
+            }
+            else
+                return (false);
+        }
+        counter++;
+    }
+    return (false);
 }
+
